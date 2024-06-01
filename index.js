69a907914ab94ac3d49a40e36774d32ef9581c50
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-02 03:29:51 +0600

    feat(mongoDB): connnect mongoDB to the server successfully

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = 3000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const dotenv = require('dotenv')
+
+
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = "mongodb+srv://stride:La3kBghqdxf0wzCS@cluster0.40hja.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -13,3 +44,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+// stride
+// La3kBghqdxf0wzCS
